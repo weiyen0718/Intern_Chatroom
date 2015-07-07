69805d2e793cac24b9130f1d6ec11ad580f789b6
weiyen0718 <wei.yen.0718@gmail.com>
2015-07-07 10:33:07 +0800

    mongo

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var mongo = require('mongodb').MongoClient;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/chatroom.html');
@@ -12,9 +14,26 @@ app.get('/chatting.html', function(req, res){
 
 
 io.on('connection', function(socket){
+
+    console.log('a user connected');
+	// mongo.connect(process.env.CUSTOMCONNSTR_MONGOLAB_URI, function (err, db) {
+ //        var collection = db.collection('chat messages')
+ //        var stream = collection.find().sort({ _id : -1 }).limit(10).stream();
+ //        stream.on('data', function (chat) { socket.emit('chat', chat); });
+ //    });
+
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    // mongo.connect(process.env.CUSTOMCONNSTR_MONGOLAB_URI, function (err, db) {
+    //         var collection = db.collection('chat messages');
+    //         collection.insert({ content: msg }, function (err, o) {
+    //             if (err) { console.warn(err.message); }
+    //             else { console.log("chat message inserted into db: " + msg); }
+    //         });
+    //         });
+  	io.emit('chat message', msg);
   });
+
+
 });
 
 http.listen(3000, function(){
